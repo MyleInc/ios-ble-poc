95c6a7fa502d432b9e6473d72f0c4cb4c64c838a
cxphong-macmini <you@example.com>
2014-11-08 17:53:29 +0700

    new branch commit

--- CBTutorial/CBCentralManagerViewController.h
+++ CBTutorial/CBCentralManagerViewController.h
@@ -1,9 +1,9 @@
 //
-//  CBCentralManagerViewController.h
-//  CBTutorial
+//  ViewController.h
+//  IOS_BLE
 //
-//  Created by Orlando Pereira on 10/8/13.
-//  Copyright (c) 2013 Mobiletuts. All rights reserved.
+//  Created by cxphong-macmini on 11/5/14.
+//  Copyright (c) 2014 cxphong. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,28 +12,18 @@
 #import "SERVICES.h"
 
 @interface CBCentralManagerViewController : UIViewController < CBCentralManagerDelegate, CBPeripheralDelegate>
-
-
-//@property (weak, nonatomic) IBOutlet UIButton *btConnect;
-@property (weak, nonatomic) IBOutlet UILabel *lbViewer;
 //@property (weak, nonatomic) IBOutlet UITextField *tfSend;
 @property (weak, nonatomic) IBOutlet UITextView *tvLog;
 //@property (weak, nonatomic) IBOutlet UIButton *btSend;
 @property (nonatomic,strong) NSFileHandle *handle;
 @property NSString *PeripheralUUID;
+@property unsigned int dataLength;
 
-- (IBAction)TextfileDone:(id)sender;
-
-- (IBAction)SendData:(id)sender;
 @property (strong, nonatomic) CBCentralManager *centralManager;
 @property (strong, nonatomic) CBPeripheral *discoveredPeripheral;
 @property (strong, nonatomic) CBPeripheralManager *peripheralManager;
 @property (strong, nonatomic) NSMutableData *data;
 
-- (IBAction)ConnectPeripheral:(id)sender;
-
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central;
-
--(NSData *) IntToNSData:(NSInteger)data;
 
 @end
--- CBTutorial/CBCentralManagerViewController.m
+++ CBTutorial/CBCentralManagerViewController.m
@@ -1,19 +1,16 @@
-//
-//  CBCentralManagerViewController.m
-//  CBTutorial
-//
-//  Created by Orlando Pereira on 10/8/13.
-//  Copyright (c) 2013 Mobiletuts. All rights reserved.
-//
-
 #import "CBCentralManagerViewController.h"
 
-NSDate *startTime;
-NSDate *stopTime;
-NSUInteger numBytesReceive;
-NSString *file;
+@interface  CBCentralManagerViewController ()
+- (void) scanPeripherals;
+- (NSData *) IntToNSData:(NSInteger)data;
+- (NSDate*)getDateFromInt:(unsigned int)l;
+
+@end
 
 @implementation CBCentralManagerViewController
+{
+    unsigned int time;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -21,17 +18,14 @@ NSString *file;
     _centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
+// Scan periphrals with specific service UUID
+- (void) scanPeripherals {
+    [_centralManager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
     
-    [_centralManager stopScan];
+    [self log:@"Scanning started"];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-
-// Scan peripheral
+// Scan peripherals
 - (void)centralManagerDidUpdateState:(CBCentralManager *)central {
     // You should test all scenarios
     if (central.state != CBCentralManagerStatePoweredOn) {
@@ -39,30 +33,25 @@ NSString *file;
     }
     
     if (central.state == CBCentralManagerStatePoweredOn) {
-        // Scan for devices
-        [_centralManager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
-        [self log:@"Scanning started"];
+        [self scanPeripherals];
     }
 }
 
 //Scan success
 - (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
     
-    if ([_PeripheralUUID isEqualToString:[peripheral.identifier UUIDString]]) {
-        NSLog(@"Same peripheral");
-        return;
-    }
-    
-    //[self log:[NSString stringWithFormat:@"UUID = %@\nName = %@\nRSSI = %@ dBm\n", [peripheral.identifier UUIDString], peripheral.name, RSSI]];
+    //[self log:[NSString stringWithFormat:@"DATA=\"%@\"", advertisementData]];
     
     if (_discoveredPeripheral != peripheral) {
+        NSLog(@"Scan success");
+
         // Save a local copy of the peripheral, so CoreBluetooth doesn't get rid of it
         _discoveredPeripheral = peripheral;
+    
+        _PeripheralUUID = [peripheral.identifier UUIDString];
+    
+        [_centralManager connectPeripheral:_discoveredPeripheral options:nil];
     }
-    
-    _PeripheralUUID = [peripheral.identifier UUIDString];
-    
-    [_centralManager connectPeripheral:_discoveredPeripheral options:nil];
 }
 
 // Scan fail
@@ -74,36 +63,34 @@ NSString *file;
 // Connect device success callback
 - (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
     [_centralManager stopScan];
-    //[self log:@"Scanning stopped"];
     [self log:@"Connected"];
-    
-    [_data setLength:0];
     
     peripheral.delegate = self;
     
-    [peripheral discoverServices:nil];
+    [peripheral discoverServices:@[[CBUUID UUIDWithString:SERVICE_UUID]]];
 }
 
 // List services
 - (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+    
     if (error) {
         [self cleanup];
         return;
     }
     
-    self.lbViewer.text = @"  ";
-
     // Discover other characteristics
     NSLog(@"Services scanned !");
     for (CBService *s in peripheral.services)
     {
-        NSLog(@"Service found : %@", s.UUID);
+        NSLog(@"Service found : \"%@\"", s.UUID);
         //[self log:[NSString stringWithFormat:@"\nService[UUID] = %@\n", s.UUID]];
     }
     
+    // List 2 characteristics
+    NSArray * characteristics = [NSArray arrayWithObjects: [CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_READ], [CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_WRITE], [CBUUID UUIDWithString:CHARACTERISTIC_UUID_CONFIG], nil];
     
     for (CBService *service in peripheral.services) {
-        [peripheral discoverCharacteristics:nil forService:service];
+        [peripheral discoverCharacteristics:characteristics forService:service];
     }
 }
 
@@ -114,18 +101,66 @@ NSString *file;
         return;
     }
     
-    int count  = 1;
     for (CBCharacteristic *characteristic in service.characteristics) {
-        
-        NSLog([NSString stringWithFormat:@"%@", characteristic.UUID]);
-        
-        //[self log:[NSString stringWithFormat:@"\n%d. Characteristics[UUID] = %@\n", count++, characteristic.UUID]];
-        
-        //if (characteristic.properties == CBCharacteristicPropertyNotify) {
-            [peripheral setNotifyValue:YES forCharacteristic:characteristic];
-        //}
+        //[self log:[NSString stringWithFormat:@"Character = %@", characteristic]];
+        [peripheral setNotifyValue:YES forCharacteristic:characteristic];
     }
+    
+    // Sync time to board
+    //    double delayInSeconds = 7.0;
+    //    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    //    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *components = [calendar components:(NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond | NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear) fromDate:[NSDate date]];
+    NSInteger hour, minute, second, day, month, year;
+    
+    hour = [components hour];
+    minute = [components minute];
+    second = [components second];
+    day = [components day];
+    month = [components month];
+    year = [components year];
+    
+    NSData *data = [[NSData alloc] init];
+    Byte *byteData = (Byte*)malloc(6);
+    byteData[0] = second & 0xff;
+    byteData[1] = minute & 0xff;
+    byteData[2] = hour & 0xff;
+    byteData[3] = day & 0xff;
+    byteData[4] = month & 0xff;
+    byteData[5] = (year - 2000) & 0xff;
+    
+    data = [NSData dataWithBytes:byteData length:6];
+    [self log:[NSString stringWithFormat:@"Send = %@", data]];
+    
+    [_discoveredPeripheral writeValue:data forCharacteristic:[[[_discoveredPeripheral.services objectAtIndex:0] characteristics] objectAtIndex:1] type:CBCharacteristicWriteWithoutResponse];
+    //    });
 }
+
+-(NSDate*)getDateFromInt:(unsigned int)l
+{
+    NSDateComponents *c = [[NSCalendar currentCalendar] components:NSUIntegerMax fromDate:[NSDate date]];
+    [c setSecond:((l & 0x1f) * 2)];
+    l = l >> 5;
+    
+    [c setMinute:l & 0x3f];
+    l = l >> 6;
+    
+    [c setHour:l & 0x1f];
+    l = l >> 5;
+    
+    [c setDay:l & 0x1f];
+    l = l >> 5;
+    
+    [c setMonth:l & 0xf];
+    l = l >> 4;
+    
+    [c setYear:(l & 0x7f)];
+    
+    NSCalendar *cal = [NSCalendar currentCalendar];
+    return [cal dateFromComponents:c];
+}
+
 
 // Result of write to peripheral
 - (void) peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
@@ -134,89 +169,64 @@ NSString *file;
 }
 
 // Update value from peripheral
-- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
-
-    /** Send back to perip[heral th number of bytes received */
-    if (characteristic.value.length != 2) { // 2 is garbage value
-        NSInteger numBytes = characteristic.value.length;
-        NSData *data = [self IntToNSData:numBytes];
-        
-        for (CBCharacteristic *characteristic in [[_discoveredPeripheral.services objectAtIndex:0] characteristics]) {
-            [_discoveredPeripheral writeValue:data forCharacteristic:characteristic
-                                         type:CBCharacteristicWriteWithoutResponse];
-        }
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    
+    // Not correct characteristics
+    if (![characteristic.UUID.UUIDString isEqualToString:CHARACTERISTIC_UUID_TO_READ])
+    {
+        return;
     }
     
-    /** Convert NSData to NSString */
-    NSString *stringFromData = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
-    //NSLog(@"%@", stringFromData);
+    // Send back to peripheral number of bytes received.
+    NSInteger numBytes = characteristic.value.length;
+    NSData *data = [self IntToNSData:numBytes];
     
-    if ([stringFromData rangeOfString:@"SDCard Name" options:NSCaseInsensitiveSearch].location != NSNotFound &&
-        stringFromData.length > 0) // start
+    [_discoveredPeripheral writeValue:data forCharacteristic:[[[_discoveredPeripheral.services objectAtIndex:0] characteristics] objectAtIndex:1] type:CBCharacteristicWriteWithoutResponse];
+    
+    if (self.dataLength == 0) // First packet
     {
-        [self log:@"Start"];
+        unsigned int ml;
         
-        /** Set bunmer of receive counter */
-        numBytesReceive = 0;
+        [characteristic.value getBytes:&ml length:4];
+        [self log:[NSString stringWithFormat:@"Read record metadata: metadata length = %d", ml]];
         
-        /** Start time calc */
-        startTime = [NSDate date];
-       
-        /** Get file name */
-        NSArray* foo = [stringFromData componentsSeparatedByString: @" "];
-        NSString* fileName = [foo objectAtIndex: 2];
-        NSLog(@"name = %@", fileName);
+        [characteristic.value getBytes:&_dataLength range:NSMakeRange(4, 4)];
+        [self log:[NSString stringWithFormat:@"Read record metadata: audio length = %d", _dataLength]];
         
-        /** Delete file if is already exist */
-        NSString *cachesFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-        NSString *path = [cachesFolder stringByAppendingPathComponent:fileName];
-        NSError *error;
-        if ([[NSFileManager defaultManager] fileExistsAtPath:path])		//Does file exist?
+        [characteristic.value getBytes:&time range:NSMakeRange(8, 4)];
+        
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setDateFormat:@"yyyy-MM-dd_HH:mm:ss"];
+        [self log:[NSString stringWithFormat:@"Read record metadata: record time = %@", [formatter stringFromDate:[self getDateFromInt:time]]]];
+        
+        [self log:@"Recieveing audio data..."];
+        
+        _data = [[NSMutableData alloc] init];
+    }
+    else if (_data.length < _dataLength)
+    {
+        [_data appendData:characteristic.value];
+        NSLog(@"len = %d", _data.length);
+        
+        if (_data.length == _dataLength)
         {
-            if (![[NSFileManager defaultManager] removeItemAtPath:path error:&error])	//Delete it
-            {
-                NSLog(@"Delete file error: %@", error);
-            }
+            [peripheral setNotifyValue:NO forCharacteristic:characteristic];
+            
+            [self log:@"Data recieved!"];
+            
+            NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+            [formatter setDateFormat:@"yyyy-MM-dd_HH-mm-ss"];
+            NSString *filePath = [NSString stringWithFormat:@"Documents/%@.wav", [formatter stringFromDate:[self getDateFromInt:time]]];
+            
+            NSString *docPath = [NSHomeDirectory() stringByAppendingPathComponent:filePath];
+            [_data writeToFile:docPath atomically:YES];
+            
+            [self log:[NSString stringWithFormat:@"File received = %@.wav", [formatter stringFromDate:[self getDateFromInt:time]]]];
+            
+            // reset
+            _dataLength = 0;
         }
-        
-        /** Create new file */
-        cachesFolder = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-        file = [cachesFolder stringByAppendingPathComponent:fileName];
-        [[NSData data] writeToFile:file options:NSDataWritingAtomic error:nil];
-        
-        /** Open file to update */
-        self.handle = [NSFileHandle fileHandleForUpdatingAtPath:file];
-    }
-    else if ([stringFromData rangeOfString:@"SDCard End" options:NSCaseInsensitiveSearch].location != NSNotFound &&
-             stringFromData.length > 0) // end
-    {
-        [self log:@"End"];
-        
-        /** Close file */
-        [self.handle closeFile];
-        
-        /** Calc time execution */
-        stopTime = [NSDate date];
-        NSTimeInterval executionTime = [stopTime timeIntervalSinceDate:startTime];
-        [self log:[NSString stringWithFormat:@"Time eslapse = %fs", executionTime]];
-        
-        /** Calc receive speed kbit/s */
-        [self log:[NSString stringWithFormat:@"Bytes received = %d Bytes", numBytesReceive]];
-        float speed =  numBytesReceive/executionTime;
-        [self log:[NSString stringWithFormat:@"Speed = %f Bytes/s", speed]];
-        [self log:[NSString stringWithFormat:@"Path: %@", file]];
-        
-    }
-    else if (stringFromData == nil) //data
-    {
-        NSLog(@"Data = %@", stringFromData);
-        
-        /** Write data into file */
-        [self.handle seekToEndOfFile];
-        [self.handle writeData:characteristic.value];
-        
-        /** Add number received bytes*/
-        numBytesReceive += characteristic.value.length;
     }
 }
 
@@ -230,14 +240,19 @@ NSString *file;
 }
 
 // Update notification from peripheral
-- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
     
-    if (![characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID_01]] &&
-        ![characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID_02]]) {
-            return;
+    // Listen only 2 characterristics
+    if (![characteristic.UUID isEqual:[CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_READ]] &&
+        ![characteristic.UUID isEqual:[CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_WRITE]])
+    {
+        return;
     }
-
-    if (!characteristic.isNotifying) {
+    
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_READ]] && !characteristic.isNotifying)
+        
+    {
         // Notification has stopped
         [self log:@"Cancel connection"];
         [_centralManager cancelPeripheralConnection:peripheral];
@@ -245,13 +260,14 @@ NSString *file;
 }
 
 // Disconnect peripheral
-- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+- (void)centralManager:(CBCentralManager *)central didDisconnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
     _discoveredPeripheral = nil;
     _PeripheralUUID = @"";
     
-     // Scan for devices again
-    if (central.state == CBCentralManagerStatePoweredOn) {
-       
+    // Scan for devices again
+    if (central.state == CBCentralManagerStatePoweredOn)
+    {
         [_centralManager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
         [self log:@"Scanning started"];
     }
@@ -265,8 +281,8 @@ NSString *file;
         for (CBService *service in _discoveredPeripheral.services) {
             if (service.characteristics != nil) {
                 for (CBCharacteristic *characteristic in service.characteristics) {
-                    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID_01]] ||
-                        [characteristic.UUID isEqual:[CBUUID UUIDWithString:TRANSFER_CHARACTERISTIC_UUID_02]]) {
+                    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_READ]] ||
+                        [characteristic.UUID isEqual:[CBUUID UUIDWithString:CHARACTERISTIC_UUID_TO_WRITE]]) {
                         if (characteristic.isNotifying) {
                             [_discoveredPeripheral setNotifyValue:NO forCharacteristic:characteristic];
                             return;
@@ -289,9 +305,7 @@ NSString *file;
     self.tvLog.text = [NSString stringWithFormat:@"%@\r\n%@: %@", self.tvLog.text, [formatter stringFromDate:[NSDate date]], s];
     
     [self.tvLog scrollRangeToVisible:NSMakeRange([self.tvLog.text length], 0)];
-   // [self.tvLog setScrollEnabled:NO];
     [self.tvLog setScrollEnabled:YES];
-    //   });
     
 }
 

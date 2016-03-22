ed15fd399c61e4b4bbcff2cc0a6929d165d63748
Mikalai Silivonik <mikalai.silivonik@outlook.com>
2016-03-21 23:36:44 -0600

    disabled reading of Audio File Stored charactersitics

--- MYLE_BLE/TapManager.m
+++ MYLE_BLE/TapManager.m
@@ -738,7 +738,7 @@ typedef struct {
                 [self sendCurrentTime];
                 
                 // check maybe there are files stored in TAP
-                [_currentPeripheral readValueForCharacteristic:_STATUS_AUDIO_FILE_STORED];
+                //[_currentPeripheral readValueForCharacteristic:_STATUS_AUDIO_FILE_STORED];
             }
         }
     }
